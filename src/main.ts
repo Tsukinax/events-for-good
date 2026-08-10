cff19a332731ab7b4f2b3c9dfc4f187eb72ee4f9
Tsukinax <hushdalanphop@gmail.com>
2026-08-10 09:19:37 +0700

    lab05 §10: add Vercel Speed Insights component (App.vue) + Analytics inject() (main.ts) (staff-check 10.7)

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,10 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'nprogress/nprogress.css'
+import { inject } from '@vercel/analytics'
 
 const app = createApp(App)
+inject()
 
 app.use(createPinia())
 app.use(router)
